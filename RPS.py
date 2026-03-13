370d662cd886cf7281e92569c6b9ef9df08d1ee6
GKGeovany <geovafrancisco3012@gmail.com>
2026-03-13 10:28:45 +0000

    Final_code

--- RPS.py
+++ RPS.py
@@ -1,10 +1,39 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play:
+        opponent_history.append(prev_play)
+    else:
+        opponent_history.clear()
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    n = 5
+    play_order = {}
 
-    return guess
+    if len(opponent_history) < n:
+        return "R"
+
+    last_five = "".join(opponent_history[-(n):])
+    
+    potential_plays = [
+        "".join(opponent_history[-(n-1):]) + "R",
+        "".join(opponent_history[-(n-1):]) + "P",
+        "".join(opponent_history[-(n-1):]) + "S",
+    ]
+
+    for i in range(len(opponent_history) - n):
+        s = "".join(opponent_history[i:i + n])
+        if s not in play_order:
+            play_order[s] = 0
+        play_order[s] += 1
+
+    sub_order = {
+        k: play_order[k]
+        for k in potential_plays if k in play_order
+    }
+
+    if not sub_order:
+        return "P"
+
+    prediction = max(sub_order, key=sub_order.get)[-1]
+
+    ideal_response = {"P": "S", "R": "P", "S": "R"}
+    
+    return ideal_response[prediction]
